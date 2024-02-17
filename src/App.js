bfbaa21784b54b86867924639ac6c41397c8863d
puru888 <papujara162@gmail.com>
2024-02-17 16:22:46 -0500

    categories created

--- src/App.js
+++ src/App.js
@@ -6,32 +6,37 @@ const App = () => {
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 1,
       title: 'Women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 1,
       title: 'Men',
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ]
 
   return (
     <div className="categories-container">
       {
-        categories.map(({title, id}) => {
+        categories.map(({title, id, imageUrl}) => {
           return (
             <div className="category-container" key={id}>
-              <div className="background-image"></div>
+              <div className="background-image" style={{backgroundImage : `url(${imageUrl})`}}></div>
               <div className="category-body-container">
                 <h2>{title}</h2>
                 <p>Shop Now</p>
