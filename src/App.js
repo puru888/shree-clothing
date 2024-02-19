5f72160fb7e939aad47050150c109bd06d3ea4fc
Purav Pujara <papujara162@gmail.com>
2024-02-19 06:17:12 -0500

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation/>}>
+      <Route index element={<Home />} />
+      <Route path='shop'/>
+      </Route>
     </Routes>
   );
 }
